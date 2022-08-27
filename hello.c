33cb857d63e15ecb86c47202fa0950a66d80764a
Alaa Ismail <aismail8@huskers.unl.edu>
2022-08-27 18:38:29 +0000

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
-    printf(Hello! I am Alaa Ismail!\n");
-    printf(Major is Global Studies and Computer Science!\n");
+    printf("Hello, I am Alaa Ismail!\n");
+    printf("Major is Global Studies and Computer Science!\n");
 
     return 0;
 }
