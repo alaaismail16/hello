c38b6631eb5ce99d7f67f5979b563667bef4a14c
Alaa Ismail <aismail8@huskers.unl.edu>
2022-08-27 18:15:03 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf(Hello! I am Alaa Ismail!\n");
+    printf(Major is Global Studies and Computer Science!\n");
+
+    return 0;
+}
