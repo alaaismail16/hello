0078d9590be6791f6c5aba4dac3b80b3abfba81c
Apinker <apinkerman2@huskers.unl.edu>
2022-08-28 16:12:21 +0000

    added my major as well

--- hello.c
+++ hello.c
@@ -7,8 +7,12 @@
 int main(int argc, char **argv) {
 
     printf("Hello, I am Alaa Ismail!\n");
+    printf("Hello,I'm Aiden Pinkerman\n");
     printf("Major is Global Studies and Computer Science!\n");
+    printf("Major is Computer Engineer\n");
     printf("Minor is Arabic and French!\n");
+    printf("I don't have a minor\n")
+    printf("Hobby, messing around on the computer\n")
 
     return 0;
 }
